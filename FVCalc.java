206594c39e6b5b1b472a05bd8aacca3d7a5476c4
Noam Bavli <noambav@gmail.com>
2024-11-04 19:17:54 +0200

    Homework finished

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,17 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		// Replace this comment with your code
+		int currentValue = Integer.parseInt(args[0]);
+		int interstRate = Integer.parseInt(args[1]);
+		double percentageRate = (double) interstRate / 100;
+		int years = Integer.parseInt(args[2]);
+
+		System.out.println(percentageRate);
+		int futureValue = (int) (currentValue * Math.pow((1 + percentageRate), years));
+
+		System.out.println("After " + years + " years $" + currentValue + " Saved at " + interstRate + "% will yield $"
+				+ futureValue);
+
 	}
 }
--- TimeFormat.java
+++ TimeFormat.java
@@ -3,15 +3,30 @@ public class TimeFormat {
 	public static void main(String[] args) {
 		// In Java, the command-line arguments args[0], args[1], ... are represented
 		// each by a string. In this program, the single "hh:mm" input is represented
-		// by the single command-line string argument args[0]. 
-		//   
+		// by the single command-line string argument args[0].
+		//
 		// The following statement handles the hours part of the input.
-		// It concatenates the empty string "" with the leftmost hour-digit. 
+		// It concatenates the empty string "" with the leftmost hour-digit.
 		// It then concatenates the resulting string with the rightmost hour-digit,
 		// and then uses parseInt to cast the resulting string as an int.
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+		// Replace this comment with the rest of your code
+		System.out.println(hours);
+		System.out.println(minutes);
+
+		String suffix = "AM";
+		String minuteText = String.valueOf(minutes);
+		String timeText = "";
+		if (minutes < 10) {
+			minuteText = "0" + String.valueOf(minutes);
+		}
+		if (hours > 12) {
+			suffix = "PM";
+			hours = hours - 12;
+		}
+		timeText = hours + ":" + minuteText + " " + suffix;
+		System.out.println(timeText);
 	}
 }
