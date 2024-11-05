b2118a0342f70314b9ec946050846dff1d7115c4
Noam Bavli <noambav@gmail.com>
2024-11-05 15:48:37 +0200

    third name now shows up first

--- Bill3.java
+++ Bill3.java
@@ -11,6 +11,6 @@ public class Bill3 {
 		double individualShare = Math.ceil(bill / 3.0);
 
 		System.out.println(
-				"Dear " + name1 + ", " + name2 + ", and " + name3 + ": pay " + individualShare + " Shekels each.");
+				"Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + individualShare + " Shekels each.");
 	}
 }
